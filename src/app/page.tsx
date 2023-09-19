7906c8759adb7e573fd2ef56a35f64e875fe2423
georgereade <georgereade@hotmail.co.uk>
2023-09-19 11:59:41 +0100

    Added email link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import Header from "./components/Header";
 import Landing from "./components/Landing";
 import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-
-// library.add(fab, faCheckSquare, faCoffee)
 
 export default function Home() {
   return (
