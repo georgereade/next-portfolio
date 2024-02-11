b92696f067a95eeca100ffdf164cb55a606c0351
georgereade <georgereade@hotmail.co.uk>
2024-02-11 21:45:06 +0000

    Updated footnote to 2024

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="container footer">
       <footer className="d-flex flex-wrap justify-content-between align-items-center pt-3 my-3 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <span className="mb-3 mb-md-0 text-white">© 2023 George Reade</span>
+          <span className="mb-3 mb-md-0 text-white">© 2024 George Reade</span>
         </div>
       </footer>
     </div>
